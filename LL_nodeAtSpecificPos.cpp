0d7ddd05ca34e4dff78bf5271dc4f2e8c68890aa
Harshi <94774731+harshi-s@users.noreply.github.com>
2023-01-05 15:37:11 +0530

    Create LL_nodeAtSpecificPos.cpp

---
+++ LL_nodeAtSpecificPos.cpp
@@ -0,0 +1,19 @@
+void insertAtPosition(Node* head, int pos, int data){
+  Node* n=new Node(data);
+  
+  if(pos<0 || pos>size) cout<<"invalid";
+  
+  else if(pos==0) insertAtHead(n);
+  
+  else if(pos==size) insertAtTail(n);
+  
+  else {
+        Node *temp=head;
+        for(int i=1;i<pos-1;i++) {
+          temp=temp->next;
+        }
+    
+        n->next=temp->next;
+        temp->next=n;
+    }
+}
