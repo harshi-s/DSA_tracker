305f689ba10ef4c4b516153d5f56fbffef377553
Harshi <94774731+harshi-s@users.noreply.github.com>
2023-01-05 17:01:14 +0530

    Create LL_removeLast.cpp

---
+++ LL_removeLast.cpp
@@ -0,0 +1,16 @@
+void removeLast () {
+  if(head){
+  if (head->next==NULL) head=NULL;
+  else{
+    Node* temp=head;
+    
+    while(temp->next->next != NULL){
+    temp=temp->next;
+    }
+    
+    Node* lastNode=temp->next;
+    temp->next=NULL;
+    free(lastNode);
+  }
+  }
+}
